a1d8480a2d20887c027a9fcada9389903a3a7661
shupeipei <shupeipei@zyeeda.com>
2015-05-25 15:16:54 +0800

    删除 jsdoc 相关代码

--- app/src/components/header.jsx
+++ app/src/components/header.jsx
@@ -4,23 +4,10 @@ import React                              from 'react';
 import {PageHeader, Button, ModalTrigger} from 'react-bootstrap';
 import ProductFormModal                   from './product-form-modal';
 
-/**
- * 头部组件, 结合 react-bootstrap 提供的组件生成页眉区域内容.
- */
 class Header extends React.Component {
-
-  /**
-   * 构造函数
-   * @param {object} props - react 组件内部存放由上层组件所传入参数的对象.
-   */
   constructor(props: any): void {
     super(props);
   }
-
-  /**
-   * react 组件返回组件内容的基本函数.
-   * @returns 组装成的一个 jsx 语法的页眉组件, 最终会被渲染成 html.
-   */
   render(): any {
     return (
       <PageHeader>
